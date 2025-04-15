141c92c708c502b0caeb4987b49e6a8a1b3b840c
Guilherme Micheletti <guilhermemicheletti@MacBook-Pro-de-Guilherme.local>
2025-04-14 23:32:35 -0300

    first commit

---
+++ src/main/java/com/example/av1/model/Cursos.java
@@ -0,0 +1,38 @@
+package com.example.av1.model;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@EntityScan("com.example.av1.model")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Cursos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    Integer cargaHoraria;
+
+    @OneToMany(mappedBy = "curso")
+    @JsonIgnore
+    private List<Alunos> alunos;
+    
+}
